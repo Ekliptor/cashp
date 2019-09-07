f3f3092bdddaa0f7e9825d6bd26e2808ee8ec5b7
Ekliptor <Ekliptor1@yandex.com>
2019-09-07 18:15:33 +0700

    started implementation of SLDPB

---
+++ tests/SlpDbTest.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+namespace Ekliptor\CashP\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Ekliptor\CashP\CashP;
+use Ekliptor\CashP\BlockchainApi\Structs\BchAddress;
+use Ekliptor\CashP\CashpOptions;
+
+final class RestBackendTest extends TestCase {
+	public function testGetTokenInfo(): void {
+		$cashp = $this->getSlpDbCashP();
+		$tokenInfo = $cashp->getBlockchain()->getTokenInfo('c4b0d62156b3fa5c8f3436079b5394f7edc1bef5dc1cd2f9d0c4d46f82cca479');
+		$this->assertNotEmpty($tokenInfo->id, "The returned SLP token has no ID");
+		$this->assertNotEmpty($tokenInfo->name, "The returned SLP token has no name");
+		
+		$details = $cashp->getBlockchain()->getSlpAddressDetails('simpleledger:qqk4nvsryxlr40fduqys60552ah5f5mvrqss5r8ez0', 'c4b0d62156b3fa5c8f3436079b5394f7edc1bef5dc1cd2f9d0c4d46f82cca479');
+		$this->assertNotEmpty($details->transactions, "Transaction IDs of address are empty.");		
+	}
+	
+	public function testAddressBalance(): void {
+		$cashp = $this->getSlpDbCashP();
+		$balance = $cashp->getBlockchain()->getAddressTokenBalance('simpleledger:qqk4nvsryxlr40fduqys60552ah5f5mvrqss5r8ez0', 'c4b0d62156b3fa5c8f3436079b5394f7edc1bef5dc1cd2f9d0c4d46f82cca479');
+		$this->assertGreaterThan(0.0, $balance, "SLP token balance on address is 0.");
+	}
+	
+	protected function getSlpDbCashP() {
+		$options = new CashpOptions();
+		$options->blockchainApiImplementation = 'SlpDbApi';
+		return new CashP($options);
+	}
+}
+?>
